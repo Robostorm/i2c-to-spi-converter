0123b06d95dfc8c726e67ea4e5c9e6afa3e3594f
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-06-22 21:34:22 -0400

    fixed queue namings

--- include/queue.h
+++ include/queue.h
@@ -1,10 +1,11 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#define MAX 0xFF
+#define QUEUE_MAX 0xFF
 
-void push(uint8_t data);
-uint8_t pop(void);
+uint8_t queue_is_full(void);
+void queue_push(uint8_t data);
+uint8_t queue_pop(void);
 
 extern uint8_t queue_array[];
 
