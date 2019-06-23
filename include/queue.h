bde847cdfc64ba3db0b18408808313ad6f234f26
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-06-22 21:35:26 -0400

    removed queue_is_full

--- include/queue.h
+++ include/queue.h
@@ -3,7 +3,6 @@
 
 #define QUEUE_MAX 0xFF
 
-uint8_t queue_is_full(void);
 void queue_push(uint8_t data);
 uint8_t queue_pop(void);
 
--- src/queue.c
+++ src/queue.c
@@ -1,21 +1,21 @@
 #include "queue.h"
 
-uint8_t queue_array[MAX];
+uint8_t queue_array[QUEUE_MAX];
 uint8_t count = 0;
 int16_t front = 0;
 int16_t rear = -1;
 
-uint8_t is_full()
+uint8_t queue_is_full()
 {
-    if(count == MAX)
+    if(count == QUEUE_MAX)
         return 1;
     else
         return 0;
 }
 
-void push(uint8_t data)
+void queue_push(uint8_t data)
 {
-    if(is_full == 0)
+    if(count != QUEUE_MAX)
     {
         if(rear == MAX - 1)
             rear = -1;
@@ -25,11 +25,11 @@ void push(uint8_t data)
     }
 }
 
-uint8_t pop()
+uint8_t queue_pop()
 {
     uint8_t data = queue_array[front++];
 
-    if(front == MAX)
+    if(front == QUEUE_MAX)
         front == 0;
 
     count--;
