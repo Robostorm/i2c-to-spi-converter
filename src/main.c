030602b0dec19febef6efcd21be84d67246f23d9
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-06-22 22:05:23 -0400

    added stuff to main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,15 @@
 
 int main()
 {
+    usart_init();
     TWI_init();
+
+    //set LED as output
+    DDRC |= (1 << PC0);
+
+    //setup serial output as stream and direct to stdout, has to be in main method for some reason
+    FILE usart_output = FDEV_SETUP_STREAM(usart_putchar, NULL, _FDEV_SETUP_WRITE);
+    stdout = &usart_output;
 
     sei();
 
