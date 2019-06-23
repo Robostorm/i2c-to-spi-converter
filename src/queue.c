780cb99651dedb97b09525016e3a2b53351a6e1b
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-06-22 21:37:30 -0400

    fixed typo in queue.c

--- src/queue.c
+++ src/queue.c
@@ -5,19 +5,11 @@ uint8_t count = 0;
 int16_t front = 0;
 int16_t rear = -1;
 
-uint8_t queue_is_full()
-{
-    if(count == QUEUE_MAX)
-        return 1;
-    else
-        return 0;
-}
-
 void queue_push(uint8_t data)
 {
     if(count != QUEUE_MAX)
     {
-        if(rear == MAX - 1)
+        if(rear == QUEUE_MAX - 1)
             rear = -1;
 
         queue_array[++rear] = data;
