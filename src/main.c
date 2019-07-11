06cacb8b4f7eec5b97b0f0ee798ebd113433dd63
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-07-10 20:17:28 -0400

    removed led stuff

--- src/main.c
+++ src/main.c
@@ -7,11 +7,6 @@
 int main()
 {
     usart_init();
-    twi_init();
-    tlc_init();
-
-    //set LED as output
-    DDRC |= (1 << PC0);
 
     //setup serial output as stream and direct to stdout, has to be in main method for some reason
     FILE usart_output = FDEV_SETUP_STREAM(usart_putchar, NULL, _FDEV_SETUP_WRITE);
