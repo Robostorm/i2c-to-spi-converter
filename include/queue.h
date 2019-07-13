dfc42071fefcf2b6d88cb82a2bc16b5b1c6f14d1
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-07-13 10:48:26 -0400

    i2c, twi working but queue issues

--- include/queue.h
+++ include/queue.h
@@ -3,7 +3,7 @@
 
 #include <avr/io.h>
 
-#define QUEUE_MAX 0xFF
+#define QUEUE_MAX 0x10
 
 void queue_push(uint8_t data);
 uint8_t queue_pop(void);
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,44 @@
 
 #define I2C_ADDR 0x16
 
+void i2c_receive_event(int num)
+{
+    while(1 < Wire.available())
+    {
+        char byte = Wire.read();
+        //Serial.println(byte);
+        queue_push(byte);
+    }
+    int x = Wire.read();
+}
+
 void setup()
 {
     Serial.begin(9600);
-    Wire.begin(I2C_ADDR);
     Tlc.init();
+    Wire.begin(I2C_ADDR);
+    Wire.onReceive(i2c_receive_event);
 }
 
 void loop()
 {
-    Tlc.set(0,4000);
-    Tlc.set(1,4095);
-    Tlc.update();
+    if(queue_count >= 2)
+    {
+        //  byte 2    byte 1
+        // 00000000 0000 0000
+        //      value   | ch
+
+        uint8_t byte1 = queue_pop();
+        uint8_t byte2 = queue_pop();
+
+        uint8_t channel = byte1 & 0x0F;
+        uint16_t value = ((byte1 & 0xF0) >> 4) + (byte2 << 4);
+
+        Serial.println(channel);
+        Serial.println(value);
+
+        Tlc.set(channel, value);
+        Tlc.update();
+    }
 }
 
